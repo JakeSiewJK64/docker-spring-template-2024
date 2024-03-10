54879447ac11d3d598e6f23cf0e0f5c890095bc1
JakeSiewJK64 <joekanesiew@outlook.com>
2024-03-10 15:58:20 +0800

    add mantine

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,7 @@
-import type { Metadata } from "next";
+import { MantineProvider } from "@mantine/core";
 import { Inter } from "next/font/google";
+import { type Metadata } from "next";
+import "@mantine/core/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
---
+++ client/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mantine/core";
+import React from "react";
+
+const page = () => {
+  return <div>
+    <Button>button</Button>
+  </div>;
+};
+
+export default page;

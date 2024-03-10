53f24328865482a0b66a0187fb1391877ffae37f
JakeSiewJK64 <joekanesiew@outlook.com>
2024-03-10 10:20:29 +0800

    change to post method for login

--- project/src/main/java/com/jakesiewjk64/project/controller/AuthenticationController.java
+++ project/src/main/java/com/jakesiewjk64/project/controller/AuthenticationController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class AuthenticationController {
 
   private final UserService userService;
 
-  @GetMapping("/login")
+  @PostMapping("/login")
   public ResponseEntity<?> login(@RequestBody AuthRequestDto request) {
     Optional<User> user = userService.findUserByEmail(request.getEmail());
     BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();

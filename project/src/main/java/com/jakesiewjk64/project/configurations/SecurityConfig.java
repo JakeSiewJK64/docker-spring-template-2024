c6fa4e645544255cdbd5684c7f68a39b5e89461b
JakeSiewJK64 <joekanesiew@outlook.com>
2024-03-17 09:50:23 +0800

    fix deprecation warning for sec. config

--- project/src/main/java/com/jakesiewjk64/project/configurations/SecurityConfig.java
+++ project/src/main/java/com/jakesiewjk64/project/configurations/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,19 +24,19 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf()
-        .disable()
-        .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**")
-        .permitAll()
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.authorizeHttpRequests(auth -> auth
+        .requestMatchers("/api/v1/auth/**").permitAll()
         .anyRequest()
-        .authenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider)
+        .authenticated());
+    // http.authorizeRequests() // Replaced .authorizeHttpRequests() with
+    // .authorizeRequests()
+    // .requestMatchers("/api/v1/auth/**").permitAll() // Updated to use antMatchers
+    // .anyRequest()
+    // .authenticated();
+    http.sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }

9a1a194cce852c1e67847076803cc852a234afde
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-01 15:44:32 +0800

    add error catch for users query hook

--- client/hooks/users.ts
+++ client/hooks/users.ts
@@ -1,7 +1,12 @@
 import getAxiosInstance from "@/utils/axiosInstance";
 
 export async function useGetAllUsers() {
-  const res = await getAxiosInstance("/users");
+  const res = await getAxiosInstance("/users").catch((err) => {
+    return {
+      data: null,
+      statusText: "Error",
+    };
+  });
   const data = res.data;
   const status = res.statusText;
 

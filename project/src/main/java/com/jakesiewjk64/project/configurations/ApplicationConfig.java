c74ccc10d720ab833e3ef289b8b533ed1522d79d
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-01 14:10:04 +0800

    remove uneeded cors configuration

--- project/src/main/java/com/jakesiewjk64/project/configurations/ApplicationConfig.java
+++ project/src/main/java/com/jakesiewjk64/project/configurations/ApplicationConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.jakesiewjk64.project.repositories.IUserRepository;
 
@@ -45,15 +43,5 @@ public class ApplicationConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
     return config.getAuthenticationManager();
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
-      }
-    };
   }
 }

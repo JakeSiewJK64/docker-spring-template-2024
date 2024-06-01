558ce40d8ec0886bcc1c9bb039a3ea4700936444
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-01 17:14:47 +0800

    update axios instance

--- client/utils/axiosInstance.ts
+++ client/utils/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { getCookie } from "./cookiesUtils";
 
-export default function getAxiosInstance(url: string) {
+export default function getAxiosInstance() {
   const bearerToken = getCookie("token");
 
-  return axios.get(url, {
+  return axios.create({
     baseURL: "http://localhost:8080/api/v1",
     headers: {
       Authorization: `Bearer ${bearerToken?.value}`,

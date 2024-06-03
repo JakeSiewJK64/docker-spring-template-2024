5d0f5df60097714ac9218e9a02ee46ed2d9fca4b
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-03 14:30:43 +0800

    ignore jwt filter if /api/v1/auth

--- project/src/main/java/com/jakesiewjk64/project/filters/JwtAuthFilter.java
+++ project/src/main/java/com/jakesiewjk64/project/filters/JwtAuthFilter.java
@@ -36,8 +36,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
     final String email;
+    final String requestPath = request.getRequestURI();
 
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+    if (authHeader == null || !authHeader.startsWith("Bearer ") || requestPath.contains("/api/v1/auth")) {
       filterChain.doFilter(request, response);
       return;
     }

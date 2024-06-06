f569542a337823b564c69a8c4b615a62876f88a0
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-07 06:43:59 +0800

    update styling of login

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -6,7 +6,7 @@ import { LoginForm } from "@/components/form";
 const Page = () => {
   return (
     <div className="flex grow">
-      <Card className="w-[50%] mx-auto mt-[25%] max-w-[30rem] p-[1rem]">
+      <Card className="w-[50%] mx-auto mt-[15%] mb-[20%] max-w-[30rem] p-[1rem]">
         <CardTitle className="text-center py-[10px]">Login</CardTitle>
         <CardContent>
           <LoginForm />

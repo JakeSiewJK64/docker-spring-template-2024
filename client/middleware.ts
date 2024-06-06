48d07a837cad33adc7863696afa108f61de77b30
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-07 06:32:37 +0800

    update middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,25 +2,36 @@ import { NextRequest, NextResponse } from "next/server";
 import { getCookie } from "./utils/cookiesUtils";
 import { getIsTokenExpired } from "./actions/auth";
 
+const DEFAULT_LOGIN_REDIRECT = "/dashboard";
+const publicRoutes = ["/"];
+const authRoutes = ["/auth/login", "/auth/register"];
+
 export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
+  const nextUrl = request.nextUrl;
+  const path = nextUrl.pathname;
   const authToken = getCookie("token");
   const tokenExpired = await getIsTokenExpired(authToken?.value);
   const authenticated = authToken && !tokenExpired;
 
-  if (path.startsWith("/auth/")) {
+  // check route path
+  const isPublicRoute = publicRoutes.includes(path);
+  const isAuthRoute = authRoutes.includes(path);
+
+  if (isAuthRoute) {
     if (authenticated) {
-      return NextResponse.redirect(new URL("/dashboard", request.url));
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-  } else {
-    if (!authenticated) {
-      return NextResponse.redirect(new URL("/auth/login", request.url));
-    }
+
+    return null;
   }
 
-  return NextResponse.next();
+  if (!authenticated && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
 }
 
 export const config = {
-  matcher: ["/dashboard", "/users", "/auth/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };

3c1b0089604ae02160f18b9f84626402f539fefb
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-07 19:37:15 +0800

    add margin dashboard

--- client/app/(protected)/dashboard/layout.tsx
+++ client/app/(protected)/dashboard/layout.tsx
@@ -10,10 +10,10 @@ const layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <>
+    <div className="m-3">
       <div>dashboard layout</div>
       {children}
-    </>
+    </div>
   );
 };
 

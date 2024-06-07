98715c350ffa1df079ace348659709bbad1036dc
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-07 19:36:53 +0800

    add dashboard route navbar

--- client/components/native/Navbar/index.tsx
+++ client/components/native/Navbar/index.tsx
@@ -4,7 +4,9 @@ import LogoutButton from "./LogoutButton";
 const Navbar = () => {
   return (
     <Card className="rounded-sm p-3 m-2 flex">
-      <p className="my-auto">My Application</p>
+      <a className="my-auto" href="/dashboard">
+        My Application
+      </a>
       <div className="ml-auto">
         <a href="/users" className="mr-5">
           Users

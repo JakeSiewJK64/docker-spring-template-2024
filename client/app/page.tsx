20815cf56be5de5b598ce93033cd3ef220201d3f
JakeSiewJK64 <joekanesiew@outlook.com>
2024-06-08 16:11:07 +0800

    update home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,3 @@
-const geHelloWorldResponse = async () => {
-  const res = await fetch("http://localhost:8080/hello");
-  return await res.text();
-};
-
 export default async function Home() {
-  const data = await geHelloWorldResponse();
-
-  return <div>{data}</div>;
+  return <>Home page is working correctly</>;
 }
